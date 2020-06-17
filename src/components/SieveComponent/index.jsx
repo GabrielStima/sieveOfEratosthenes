08734f4b5691f54ebe4e56f18f81ab18ac60c8fb
Gabriel Stimamiglio <gabriel.stimamiglio@acertsis.com.br>
2020-06-17 19:51:12 -0300

    sieveComponent in progress

--- src/components/SieveComponent/index.jsx
+++ src/components/SieveComponent/index.jsx
@@ -1,27 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./style.css";
+
 function SieveComponent({ selectLang }) {
+  const [buttonTextBR, setButtonTextBR] = useState("Iniciar");
+  const [buttonTextEN, setButtonTextEN] = useState("Start");
+  const [finalNumber, setFinalNumber] = useState(0);
+  let handleInputEvent = null;
+
+  function handleFinalNumber(event) {
+    event.persist();
+    clearTimeout(handleFinalNumber);
+
+    handleFinalNumber = setTimeout(() => {
+      setFinalNumber(event.target.value);
+      console.log("teste", finalNumber);
+    }, 500);
+  }
+
   return (
     <>
-      <div>
+      <div className="sieveComponentBody">
         <label
           htmlFor="finalNumber"
           className="initialDescription"
           id="labelFinalNumber"
         >
-          To start, type a number greater than 2.
+          {!selectLang
+            ? "To start, type a number greater than 2."
+            : "Para iniciarmos digite um numero maior que 2."}
           <span className="testHint">
-            <b>(For a better presentation I suggest the number 50)</b>
+            <b>
+              {!selectLang
+                ? "(For a better presentation I suggest the number 50)"
+                : "(Para uma melhor apresentação sugiro o numero 50)"}
+            </b>
           </span>
         </label>
         <input
           type="number"
           name="finalNumber"
+          className="principalInput"
           id="finalNumber"
-          placeholder="Enter a number greater than 2"
+          placeholder={
+            !selectLang
+              ? "Enter a number greater than 2"
+              : "Digite um numero maior que 2"
+          }
+          onInput={handleFinalNumber}
         />
         {/*  */}
-        <button>Start</button>
+        <button className="principalButton">
+          {!selectLang ? buttonTextEN : buttonTextBR}
+        </button>
       </div>
     </>
   );
